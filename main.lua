8dc3a81c6c556108b9947b421c5c94b560e6ce72
CHADIKUS <69522892+CHADIKUS@users.noreply.github.com>
2023-08-09 11:31:29 -0300

    Add files via upload

--- main.lua
+++ main.lua
@@ -8,11 +8,11 @@ local readline = require("readline")
 
 local client = discordia.Client()
 
-local TOKEN="USER TOKEN HERE"
+local TOKEN=""
 
 --discord doesnt allow "unverified bots" to view the contents of a message, but still recieves it, so i will detect when a message is sent then get the most recent message with a request
 
-local CURRENT_CHANNEL = "TEXT CHANNEL ID"
+local CURRENT_CHANNEL = "CHANNEL ID"
 
 
 client:on("ready", function()
@@ -75,6 +75,7 @@ function getMessage(channel_id, author, channel_name, guild_name)
 end
 
 
+
 function sendMessage(channel_id, message)
     coroutine.wrap(function()
         local url = "https://discord.com/api/v8/channels/"..channel_id.."/messages"
@@ -87,23 +88,83 @@ function sendMessage(channel_id, message)
         end
         print("message sent!")
     end)()    
-
 end
 
-local prompt = ""
-local history = readline.History.new()
-local editor = readline.Editor.new({stdin = process.stdin.handle, stdout = process.stdout.handle, history = history})
-local function onLine(err, line, ...)
-    if line then
-        sendMessage(CURRENT_CHANNEL, line)
-        editor:readLine(prompt, onLine)
+function loginToDiscord(Email, Password)
+
+    local url = "https://discord.com/api/v8/auth/login"
+    local header = {{"Content-Type","application/json"}}
+    local payload = json.encode{email = Email, password = Password}
+
+    local res, body = coro.request("POST", url, header, payload, 500)
+    if res.code < 200 or res.code >= 300 or json.decode(body).token == nil then
+        print("FAILED TO LOGIN: " .. res.reason .. ": " .. res.code)
+        print("please login with token and try again")
     else
-        process:exit()
+        print("loggin successfull")
+        print()
+    
+        TOKEN =tostring(json.decode(body).token)
+
     end
 end
 
 
 
-client:run(TOKEN,{afk=true})
-editor:readLine(prompt, onLine)
+
+
+function init(email, password)
+
+    loginToDiscord(email, password)
+    client:run(TOKEN,{afk=true})
+
+    local prompt = ""
+    local history = readline.History.new()
+    local editor = readline.Editor.new({stdin = process.stdin.handle, stdout = process.stdout.handle, history = history})
+    local function onLine(err, line, ...)
+        if line then
+            sendMessage(CURRENT_CHANNEL, line)
+            editor:readLine(prompt, onLine)
+        else
+            process:exit()
+        end
+    end
+
+    editor:readLine(prompt, onLine)
+
+end
+
+print("LOGIN")
+
+-- local state = 0
+-- local email = ""
+-- local password = ""
+
+
+-- local init_prompt = ""
+-- local init_editor = readline.Editor.new({stdin = process.stdin.handle, stdout = process.stdout.handle})
+-- local function init_onLine(err, line, ...)
+--     if line then
+--         if state == 0 then
+--             email = line
+--             state = 1
+--             print("PASSWORD")
+--             init_editor:readLine(init_prompt, init_onLine)
+--         elseif state == 1 then
+--             password = line
+--             state = 2
+--             print(email)
+--             print(password)
+--             init(email, password)
+--         end
+--     else
+--         --process:exit()
+--     end
+-- end
+-- print("ENTER EMAIL")
+-- init_editor:readLine(init_prompt, init_onLine)
+
+
+init("EMAIL","PASSWORD")
+
 --client:run(BOT_TOKEN)
